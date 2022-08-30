eb7a320937083a2e40fb2d1f2fc265d2890c14c1
VuksanFilip <filipvuksan.iphone@gmail.com>
2022-08-30 17:34:05 +0200

    New: Test files

--- main.go
+++ main.go
@@ -1,58 +1,89 @@
 package main
 
 import (
-	"Projekat/Structures"
+	record "Projekat/Structures"
+	"Projekat/Structures/SSTable"
 	"fmt"
+	"strconv"
+	"time"
 )
 
 func main() {
-	app := Structures.CreateApp()
-	fmt.Println("KORISNICKI MENI")
-	for {
-		fmt.Println("1) PUT\n" +
-			"2) GET\n" +
-			"3) DELETE\n" +
-			"4) Exit")
-		var option string
-		fmt.Println("Izaberite odgovarajucu opciju: ")
-		fmt.Scanln(&option)
-		if option == "1" {
-			var key string
-			var value string
-			fmt.Println("Unesite kljuc: ")
-			fmt.Scanln(&key)
-			fmt.Println("Unesite vrednost: ")
-			fmt.Scanln(&value)
-			success := app.Put(key, []byte(value))
-			if success == true {
-				fmt.Println("Element je uspesno unet.")
-			} else {
-				fmt.Println("Element nije uspesno unet.")
-			}
-		} else if option == "2" {
-			var key string
-			fmt.Println("Unesite kljuc: ")
-			fmt.Scanln(&key)
-			value := app.Get(key)
-			if value == nil {
-				fmt.Println("Element sa unetim kljucem ne postoji.")
-			} else {
-				fmt.Println("Vrednost elementa sa unetim kljucem je: ", value)
-			}
-		} else if option == "3" {
-			var key string
-			fmt.Println("Unesite kljuc: ")
-			fmt.Scanln(&key)
-			success := app.Delete(key)
-			if success == true {
-				fmt.Println("Element je uspesno obrisan.")
-			} else {
-				fmt.Println("Element nije uspesno obrisan.")
-			}
-		} else if option == "4" {
-			break
-		} else {
-			fmt.Println("Uneli ste nepostojecu opciju! Pokusajte ponovo!")
-		}
+	fmt.Println()
+	records := []record.Record{}
+
+	start := time.Now()
+
+	for i := 0; i < 100; i++ {
+		bytes := []byte{1, 2}
+		record := record.CreateRecord(strconv.Itoa(i), bytes, 0)
+		records = append(records, *record)
 	}
+
+	dataFilePath := "Data/Data/data.gob"
+	indexFilePath := "Data/Index/index.gob"
+	summaryFilePath := "Data/Summary/summary.gob"
+	filterFilePath := "Data/Filter/filter.gob"
+
+	sstable := SSTable.SSTable{}
+	sstable.DataFilePath = "Data/Data/data.gob"
+	sstable.IndexFilePath = "Data/Index/index.gob"
+	sstable.SummaryFilePath = "Data/Summary/summary.gob"
+	sstable.FilterFilePath = "Data/Filter/filter.gob"
+
+	//SSTableProba.TestBloomFilter()
+	SSTable.CreateSSTable(dataFilePath, indexFilePath, summaryFilePath, filterFilePath, records)
+	_, found := sstable.GetRecordInSStableForKey("1")
+	fmt.Println(found)
+	end := time.Since(start)
+	fmt.Println(end)
+	//app := Structures.CreateApp()
+	//fmt.Println("KORISNICKI MENI")
+	//for {
+	//	fmt.Println("1) PUT\n" +
+	//		"2) GET\n" +
+	//		"3) DELETE\n" +
+	//		"4) Exit")
+	//	var option string
+	//	fmt.Println("Izaberite odgovarajucu opciju: ")
+	//	fmt.Scanln(&option)
+	//	if option == "1" {
+	//		var key string
+	//		var value string
+	//		fmt.Println("Unesite kljuc: ")
+	//		fmt.Scanln(&key)
+	//		fmt.Println("Unesite vrednost: ")
+	//		fmt.Scanln(&value)
+	//		success := app.Put(key, []byte(value))
+	//		if success == true {
+	//			fmt.Println("Element je uspesno unet.")
+	//		} else {
+	//			fmt.Println("Element nije uspesno unet.")
+	//		}
+	//	} else if option == "2" {
+	//		var key string
+	//		fmt.Println("Unesite kljuc: ")
+	//		fmt.Scanln(&key)
+	//		value := app.Get(key)
+	//		if value == nil {
+	//			fmt.Println("Element sa unetim kljucem ne postoji.")
+	//		} else {
+	//			fmt.Println("Vrednost elementa sa unetim kljucem je: ", value)
+	//		}
+	//	} else if option == "3" {
+	//		var key string
+	//		fmt.Println("Unesite kljuc: ")
+	//		fmt.Scanln(&key)
+	//		success := app.Delete(key)
+	//		if success == true {
+	//			fmt.Println("Element je uspesno obrisan.")
+	//		} else {
+	//			fmt.Println("Element nije uspesno obrisan.")
+	//		}
+	//	} else if option == "4" {
+	//		break
+	//	} else {
+	//		fmt.Println("Uneli ste nepostojecu opciju! Pokusajte ponovo!")
+	//	}
+	//}
 }
